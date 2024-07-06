ee214c6a0e5d93f3bca2f5d5520863fbd9ada3e2
Killian <killian.boularand@outlook.fr>
2024-07-06 12:34:38 +0200

    rrtr

--- auth.ts
+++ auth.ts
@@ -14,15 +14,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma), // adaptateur prisma 
   ...authConfig, // config creer dans auth.config.ts 
   callbacks: {
-    jwt({ token, user }) {
-      if (user) { // User is available during sign-in
-        token.id = user.id
-      }
-      return token
+     jwt({ token, user }) {
+      return { ...token, ...user }; 
     },
     session({ session, token }) {
-      session.user.id = token.id as string
-      return session
+      session.user.role = token.role as string; // on passe le role au token
+      return session;
     },
     /* authorized: async ({ auth }) => {
       // Les utilisateurs connectés sont authentifiés, sinon redirigés vers la page de connexion
