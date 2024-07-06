501213229f0822a5e2566c63d41e0148d36028a9
Killian <killian.boularand@outlook.fr>
2024-07-06 14:44:18 +0200

    dfdhfg

--- auth.ts
+++ auth.ts
@@ -11,19 +11,11 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma), // adaptateur prisma 
   ...authConfig, // config creer dans auth.config.ts 
   callbacks: {
-    jwt({ token, user }) {
-      if (user) { // User is available during sign-in
-        token.sub = user.id
+    session: async ({session, user}) => {
+      if (session.user) {
+        session.user.id = user.id
       }
-      return token
-    },
-    session({ session, token }) {
-      session.user.role = token.role as string // on passe le role au token
-      return session;
-    },
-    /* authorized: async ({ auth }) => {
-      // Les utilisateurs connectés sont authentifiés, sinon redirigés vers la page de connexion
-      return !!auth
-    }, */
+      return session
+    }
   },
 });
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,15 +1,9 @@
-// On etant les info recu de la connection pour y rajouter le role sur le token
-import NextAuth from "next-auth";
-import { User } from "@prisma/client";
-import { JWT } from "next-auth/jwt";
+import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
- 
-  interface Session {
-    user: User;
+  interface Session extends DefaultSession {
+    user: DefaultUser & {
+      id: string
+    }
   }
-}
-
-declare module "next-auth/jwt" {
-  type JWT = User;
 }
